f31d20b3438c0450eb1de6bad03b677ce6fdf021
natraj <n21r20@gmail.com>
2023-11-17 13:24:09 -0800

    Added a security group and a web server

--- main.tf
+++ main.tf
@@ -5,9 +5,43 @@ provider "aws" {
 resource "aws_instance" "example" {
    ami		= "ami-051fd28403c0dd170"
    instance_type = "t2.micro"
+   vpc_security_group_ids = [aws_security_group.instance.id]
+
+   user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+  
+  user_data_replace_on_change = true
 
    tags = {
-      Name = "first terraform example"
+      Name = "terraform-example"
    }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+  
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public address of the web server"
+  
 }
 
